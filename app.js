d6288ea9c033c196208b49c6bb4a3b0af0419df7
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2021-01-19 02:37:24 +0300

    apiRouter

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -16,5 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/api', apiRouter);
 
 module.exports = app;
