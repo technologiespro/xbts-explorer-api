24bdc70744250cbbfd360b61f7b6fed9fcd938f3
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2021-01-21 05:50:44 +0300

    app port

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const jsonFile = require('jsonfile');
+const config = jsonFile.readFileSync('./config.json');
+
+process.env.PORT = config.port;
+console.log("Running on port:", process.env.PORT);
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const apiRouter = require('./routes/api');
