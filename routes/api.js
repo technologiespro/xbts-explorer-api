fadc7bac4ac07596ae6937b02b201c2073879f50
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2021-01-21 07:49:44 +0300

    tx

--- routes/api.js
+++ routes/api.js
@@ -14,16 +14,24 @@ async function startAfterConnected() {
 }
 
 async function callEachBlock(obj) {
-    console.log(obj)
+    console.log(obj[0])
+    const witness = await BitShares.db.get_objects([obj[0].current_witness]);
+    const witnessAccount = await BitShares.db.get_objects([witness[0].witness_account]);
+    console.log('witnessAccount', witnessAccount[0].name);
+
     emitter.eventBus.sendEvent('block:response', {
         op: 'block:response',
-        data: obj[0],
+        data: {
+            block: obj[0],
+            tx: (await BitShares.db.get_block(obj[0].head_block_number)).transactions,
+            witness: {
+                name: witnessAccount[0].name,
+                data: witness[0]
+            }
+        },
     });
 
-    let result = await BitShares.db.get_block(
-        obj[0].head_block_number
-    );
-    console.log(result)
+    //console.log(result)
 }
 
 router.get('/test', async function (req, res, next) {
