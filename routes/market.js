688188c186210a41abef304478adb1be59599886
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2021-04-20 20:20:20 +0300

    price-paprika

--- routes/market.js
+++ routes/market.js
@@ -4,6 +4,34 @@ const axios = require('axios');
 
 let cacheTickers = {};
 
+async function getPricePaprika(ticker) {
+    let data = await axios.get('https://api.coinpaprika.com/v1/tickers/' + ticker + '?quotes=rub,eur,usd,cny');
+    return data.data
+}
+
+router.get('/price-paprika/:ticker', async function (req, res, next) {
+    let dt = Math.floor(Date.now() / 1000) - 60;
+
+    if (!cacheTickers[req.params["ticker"]]) {
+        cacheTickers[req.params["ticker"]]= {};
+        cacheTickers[req.params["ticker"]].timestamp = 0
+
+    }
+
+    if (cacheTickers[req.params["ticker"]].timestamp < dt) {
+        cacheTickers[req.params["ticker"]] = {
+            timestamp: Math.floor(Date.now() / 1000),
+            data: await getPricePaprika(req.params["ticker"])
+        }
+    } else {
+        //console.log('cache', req.params["ticker"])
+    }
+
+    await res.json(cacheTickers[req.params["ticker"]].data)
+
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
