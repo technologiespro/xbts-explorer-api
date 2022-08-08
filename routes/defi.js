44ca5845c054ebc45ba0fdab6c55aeb0262b05ea
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2022-08-09 01:19:51 +0300

    add defi

---
+++ routes/defi.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/price', async function(req, res, next) {
+
+
+});
+
+module.exports = router;
